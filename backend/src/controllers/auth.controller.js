575d5a9c1aa0aeb5f7c76e33bb43bc3550498c9d
joseLara108 <jlara@data108.com>
2024-10-31 12:17:19 -0300

    commit v3

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -29,7 +29,7 @@ exports.signup = (req, res) => {
         }
     });
 };
-
+// aqui hacer cambios
 exports.signin = (req, res) => {
     const { email, password } = req.body;
     User.findByEmail(email.trim(), (err, data) => {
